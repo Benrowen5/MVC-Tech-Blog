6ccd6e2dbfb2a873a1f542fe2a9b8dfe7fbcb4a5
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-11-30 13:27:55 -0500

    added homeroute to render posts on load

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,10 +3,22 @@ const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: {
+            model: User,
+            attributes: ['username']
+        }
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({plain: true}));
         res.render('homepage', {
-        
+            posts,
+            loggedIn: req.session.loggedIn
+        });
     })
     .catch(err => {
+        console.log(err);
         res.status(500).json(err);
     });
 });
